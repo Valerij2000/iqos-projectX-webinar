e665ad29122fb09220caceaf6d46a59b7c9c0f3c
Валерий <valery.shumkov@mail.ru>
2024-06-18 10:27:02 +0400

    updated

--- dev/js/blocks/main.js
+++ dev/js/blocks/main.js
@@ -1,13 +1,20 @@
 let timer;
 let x = 15;
+let totalTime = 60000;
 countdown();
 
-function countdown() {
-  document.getElementById('timer').innerHTML = x + ' Минут';
-  x--;
+function countdown() {  
   if (x < 0) {
     clearTimeout(timer); // timer stop on zero
+  } else if (x <= 4 && x > 1) {
+    timer = setTimeout(countdown, totalTime);
+    document.getElementById('timer').innerHTML = x + ' Минуты';
+  } else if (x == 1) {
+    timer = setTimeout(countdown, totalTime);
+    document.getElementById('timer').innerHTML = x + ' Минута';
   } else {
-    timer = setTimeout(countdown, 60000);
+    timer = setTimeout(countdown, totalTime);
+    document.getElementById('timer').innerHTML = x + ' Минут';
   }
+  x--;
 }
